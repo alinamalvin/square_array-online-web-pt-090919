81248c42ec10be808aa691057b5db5a6c5b8525b
Alina Kornilova <alinakornilova78@gmail.com>
2019-09-17 17:50:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
-  array=[]
-  array.each do |i|
-    i ** 2 
-  end
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
+
+my_arr = [1, 2]
+p square_array(my_arr) #=> [1, 4]
